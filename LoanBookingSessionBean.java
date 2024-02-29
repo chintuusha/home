6b9206ecb508060f1d6aabe272a4571cf01ab88b
root <root@ip-172-31-42-237.ec2.internal>
2024-02-29 08:16:28 +0000

    this is  my secnd commut

--- LoanBookingSessionBean.java
+++ LoanBookingSessionBean.java
@@ -34,4 +34,16 @@ System.out.println();
 //function calling  
 printPattern(n);  
 }  
+public class SpiralPatternExample2
+{
+//function to print the spiral pattern
+public static void printPattern(int n)
+{
+//detrmines the boundary size of the array
+int size = 2 * n - 1;
+//inner loop
+for(int i = 1; i <= size; i++)
+{
+//outer loop
+for(int j = 1; j <= size; j++)
 }  
